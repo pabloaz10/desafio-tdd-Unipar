c8477836cee9e0cee335896955704247855067a7
pabloaz10 <pablo.pmza@gmail.com>
2025-08-20 20:17:42 -0300

    Primeiros testes passados

--- venda.js
+++ venda.js
@@ -1,6 +1,14 @@
 
-
 function calcularVenda(venda) {
-  // Implementação da função
+  const subtotal = venda.itens.reduce((acumulador, item) => {
+    const descontoDoItem = item.valorItem * (item.descontoPercentual / 100) + item.descontoReal;
+    const valorFinalDoItem = item.valorItem - descontoDoItem;
+    return acumulador + valorFinalDoItem;
+  }, 0);
+  const descontoTotal = subtotal * (venda.descontoTotalPercentual / 100) + venda.descontoTotalReal;
+  const totalFinal = subtotal - descontoTotal;
+  return {
+    totalFinal: totalFinal,
+  };
 }
 module.exports = calcularVenda;
